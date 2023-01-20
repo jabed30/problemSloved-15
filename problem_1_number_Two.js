d40f7140ca7ccdc22a5a26403617c1fa5673d123
jabed30 <jabedlxp@gmail.com>
2023-01-21 00:44:53 +0600

    3 Problem solved

---
+++ problem_1_number_Two.js
@@ -0,0 +1,6 @@
+function addition(fristNumber, secondNumber){
+    return fristNumber+secondNumber;
+}
+   console.log(addition(3,2));
+   console.log(addition(-3, -6));
+   console.log(addition(7, 3));
---
+++ problem_2_convert.js
@@ -0,0 +1,6 @@
+   function convert(minutes, seconds){
+    return minutes*seconds;
+   }
+   console.log(convert(5,60));
+   console.log(convert(3, 60));
+   console.log(convert(2, 60));
